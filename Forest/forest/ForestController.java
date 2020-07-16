21a36d5bbdbdecaa09b5c29b75e0543c56545056
Miyajima Kenta <shadowlink0122@gmail.com>
2020-07-16 18:37:29 +0900

    Create skeleton of a Forest's Controller.

---
+++ Forest/forest/ForestController.java
@@ -0,0 +1,68 @@
+public class Controller {
+
+	private Point current;
+
+	private Model model;
+
+	private Point previous;
+
+	private View view;
+
+	private View view;
+
+	private Model model;
+
+	private MouseInputAdapter mouseInputAdapter;
+
+	public Controller() {
+
+	}
+
+	public void mouseClicked(MouseEvent aMouseEvent) {
+
+	}
+
+	/**
+	 *  
+	 */
+	public void mouseDragged(MouseEvent aMouseEvent) {
+
+	}
+
+	public void mouseEntered(MouseEvent aMouseEvent) {
+
+	}
+
+	public void mouseExited(MouseEvent aMouseEvent) {
+
+	}
+
+	public void mouseMoved(MouseEvent aMouseEvent) {
+
+	}
+
+	public void mousePressed(MouseEvent aMouseEvent) {
+
+	}
+
+	public void mouseReleased(MouseEvent aMouseEvent) {
+
+	}
+
+	public void mouseWheelMoved(MouseWheelEvent aMouseWheelEvent) {
+
+	}
+
+	public void setModel(Model aModel) {
+
+	}
+
+	public void setView(View aView) {
+
+	}
+
+	public String toString() {
+		return null;
+	}
+
+}
