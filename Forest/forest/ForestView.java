427d2d0058f8fc1503285cf7d8e00e53bf4c3d31
Miyajima Kenta <shadowlink0122@gmail.com>
2020-07-16 18:38:13 +0900

    Create skeleton of a Forest's View.

---
+++ Forest/forest/ForestView.java
@@ -0,0 +1,58 @@
+import java.util.Collection;
+
+public class View {
+
+	private Controller controller;
+
+	private Model model;
+
+	private Point offset;
+
+	private Controller controller;
+
+	private Model model;
+
+	private Model model;
+
+	private Collection<FramePoint> framePoint;
+
+	/**
+	 *  
+	 */
+	public void paintComponent(Graphics aGraphics) {
+
+	}
+
+	/**
+	 *  
+	 */
+	public void scrollBy(Point aPoint) {
+
+	}
+
+	/**
+	 *  
+	 */
+	public void scrollTo(Point aPoint) {
+
+	}
+
+	/**
+	 *  
+	 */
+	public String toString() {
+		return null;
+	}
+
+	public void update() {
+
+	}
+
+	/**
+	 *  
+	 */
+	public Node whichOfNodes(int aPoint Point) {
+		return null;
+	}
+
+}
